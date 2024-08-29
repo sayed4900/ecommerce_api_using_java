f3ef9d56ed81a7a18e6a51719dde0c095d1c514a
sayed4900 <sayedabdo4900@gmail.com>
2024-08-30 02:03:33 +0300

    init

---
+++ src/main/java/com/sayed/dreamshops/DreamshopsApplication.java
@@ -0,0 +1,13 @@
+package com.sayed.dreamshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamshopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamshopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayed/dreamshops/DreamshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayed.dreamshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamshopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
